5c6dbb64071fcdc5a36ee73af7f1ebffb1891fef
freya02 <41875020+freya022@users.noreply.github.com>
2025-02-06 14:07:13 +0100

    Add react-router

---
+++ frontend/src/ErrorPage.tsx
@@ -0,0 +1,29 @@
+import { isRouteErrorResponse, useRouteError } from "react-router";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  // https://stackoverflow.com/a/76126878
+  let errorMessage: string;
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.data?.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-y-5">
+      <h1 className="text-4xl font-semibold">Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p className="dark:text-gray-500">
+        <i>{errorMessage}</i>
+      </p>
+    </div>
+  );
+}
---
+++ frontend/src/Root.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Link } from "react-router";
+
+export default function Root() {
+  return (
+    <>
+      <div className="flex items-center px-4 py-2 dark:bg-neutral-950">
+        <div className="grow">
+          <Link to={`/`} className="flex max-w-min items-center gap-x-1">
+            <img
+              alt="Commandinator Logo"
+              src="/logo-round.svg"
+              className="size-8"
+            />
+            <span className="grow">Commandinator</span>
+          </Link>
+        </div>
+        <div>avatar</div>
+      </div>
+      <div className="h-auto p-2">
+        <span>content</span>
+      </div>
+    </>
+  );
+}
