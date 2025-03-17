8a644e53d34abc99c00d2d445f210d7910d2d566
freya02 <41875020+freya022@users.noreply.github.com>
2025-03-17 12:34:42 +0100

    api: Test updating config
    Tbh there's no reason this fails if the mapping works,
    it just does a full replacement, not even patching

--- backend/api/src/test/kotlin/dev/freya02/commandinator/api/RolesConfigTests.kt
+++ backend/api/src/test/kotlin/dev/freya02/commandinator/api/RolesConfigTests.kt
@@ -1,6 +1,9 @@
 package dev.freya02.commandinator.api
 
+import dev.freya02.commandinator.api.dto.RolesConfigDTO
 import dev.freya02.commandinator.api.entity.*
+import dev.freya02.commandinator.api.mapper.RolesConfigMapper
+import dev.freya02.commandinator.api.mapper.mapper
 import dev.freya02.commandinator.api.repository.RolesConfigRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -63,5 +66,49 @@ class RolesConfigTests(
         )
 
         assertDoesNotThrow { rolesConfigRepository.findByGuildId(GUILD_ID) }
+    }
+
+    @Test
+    fun `Update RolesConfig`() {
+        val mapper = mapper<RolesConfigMapper>()
+        val oldDto = RolesConfigDTO(
+            listOf(
+                RolesConfigDTO.Message(
+                    "content",
+                    listOf(
+                        RolesConfigDTO.Message.Row(
+                            RolesConfigDTO.Message.Button(
+                                roleName = "BC Updates",
+                                RolesConfigDTO.Message.Button.Style.SUCCESS,
+                                label = "label",
+                                emoji = RolesConfigDTO.Message.UnicodeEmoji("bell"),
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        rolesConfigRepository.save(mapper.toRolesConfig(oldDto, GUILD_ID))
+
+        val newDto = RolesConfigDTO(
+            listOf(
+                RolesConfigDTO.Message(
+                    "Use this button to toggle <@&{roleId[BC Updates]}>",
+                    listOf(
+                        RolesConfigDTO.Message.Row(
+                            RolesConfigDTO.Message.Button(
+                                roleName = "BC Updates",
+                                RolesConfigDTO.Message.Button.Style.SUCCESS,
+                                label = "Toggle BC update pings",
+                                emoji = RolesConfigDTO.Message.UnicodeEmoji("bell"),
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        rolesConfigRepository.save(mapper.toRolesConfig(newDto, GUILD_ID))
     }
 }
