850f4ed55c297ff5d0868f2cd64b1a58743eb7e2
freya02 <41875020+freya022@users.noreply.github.com>
2025-03-17 21:28:39 +0100

    api: Test RolesConfigController

---
+++ backend/api/src/test/kotlin/dev/freya02/commandinator/api/RolesConfigControllerTests.kt
@@ -0,0 +1,78 @@
+package dev.freya02.commandinator.api
+
+import com.ninjasquad.springmockk.MockkBean
+import dev.freya02.commandinator.api.bot.BotClient
+import dev.freya02.commandinator.api.controllers.RolesConfigController
+import dev.freya02.commandinator.api.service.RolesConfigService
+import io.mockk.every
+import io.mockk.just
+import io.mockk.runs
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+private const val EXAMPLE_GUILD_ID: Long = 1568581531818
+private const val EXAMPLE_USER_ID: Long = 6186188134619
+
+@WebMvcTest(controllers = [RolesConfigController::class])
+class RolesConfigControllerTests @Autowired constructor(
+    private val mockMvc: MockMvc,
+) {
+
+    @MockkBean
+    lateinit var rolesConfigService: RolesConfigService
+
+    @MockkBean
+    lateinit var botClient: BotClient
+
+    @Test
+    fun `Must be in guild to create config`() {
+        every { botClient.isInGuild(any(), any()) } returns false
+
+        mockMvc.post("/api/guilds/$EXAMPLE_GUILD_ID/roles") {
+            // https://docs.spring.io/spring-security/reference/servlet/test/mockmvc/csrf.html
+            with(csrf().asHeader())
+            // https://docs.spring.io/spring-security/reference/servlet/test/mockmvc/oauth2.html#testing-oauth2-login
+            with(oauth2Login().attributes {
+                it["id"] = "1234"
+            })
+
+            contentType = MediaType.APPLICATION_JSON
+            //language=json
+            content = """
+                {
+                    "messages": []
+                }
+            """.trimIndent()
+        }.andExpect {
+            status { isForbidden() }
+        }
+
+        every { rolesConfigService.createConfig(any(), any()) } just runs
+        every { botClient.isInGuild(EXAMPLE_GUILD_ID, EXAMPLE_USER_ID) } returns true
+
+        mockMvc.post("/api/guilds/$EXAMPLE_GUILD_ID/roles") {
+            // https://docs.spring.io/spring-security/reference/servlet/test/mockmvc/csrf.html
+            with(csrf().asHeader())
+            // https://docs.spring.io/spring-security/reference/servlet/test/mockmvc/oauth2.html#testing-oauth2-login
+            with(oauth2Login().attributes {
+                it["id"] = EXAMPLE_USER_ID.toString()
+            })
+
+            contentType = MediaType.APPLICATION_JSON
+            //language=json
+            content = """
+                {
+                    "messages": []
+                }
+            """.trimIndent()
+        }.andExpect {
+            status { isOk() }
+        }
+    }
+}
