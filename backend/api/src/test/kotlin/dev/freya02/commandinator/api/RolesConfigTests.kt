37bc68874fa424cc479632978961c6bd2c5fa106
freya02 <41875020+freya022@users.noreply.github.com>
2025-03-17 12:27:44 +0100

    api: Small test cleanup

--- backend/api/src/test/kotlin/dev/freya02/commandinator/api/RolesConfigTests.kt
+++ backend/api/src/test/kotlin/dev/freya02/commandinator/api/RolesConfigTests.kt
@@ -20,40 +20,38 @@ class RolesConfigTests(
     // No need to delete or make sure exceptions don't prevent cleanup, as transactions are rolled back
     @Test
     fun `Insert and retrieve RolesConfig`() {
-        val config = assertDoesNotThrow {
-            rolesConfigRepository.save(
-                RolesConfig(
-                    guildId = GUILD_ID,
-                    messages = arrayListOf(
-                        RoleMessage(
-                            content = "content",
-                            components = arrayListOf(
-                                RoleMessageRow(
-                                    components = arrayListOf(
-                                        RoleMessageButton(
-                                            roleName = "BC Updates",
-                                            style = ButtonStyle.SUCCESS,
-                                            label = "Toggle BC update pings",
-                                            emoji = UnicodeEmoji("\uD83D\uDD14")
-                                        )
+        val config = rolesConfigRepository.save(
+            RolesConfig(
+                guildId = GUILD_ID,
+                messages = arrayListOf(
+                    RoleMessage(
+                        content = "content",
+                        components = arrayListOf(
+                            RoleMessageRow(
+                                components = arrayListOf(
+                                    RoleMessageButton(
+                                        roleName = "BC Updates",
+                                        style = ButtonStyle.SUCCESS,
+                                        label = "Toggle BC update pings",
+                                        emoji = UnicodeEmoji("\uD83D\uDD14")
                                     )
-                                ),
-                                RoleMessageRow(
-                                    components = arrayListOf(
-                                        RoleMessageSelectMenu(
-                                            placeholder = null,
-                                            choices = arrayListOf(
-                                                RoleMessageSelectMenuChoice(
-                                                    roleName = "V3",
-                                                    label = "V3",
-                                                    emoji = UnicodeEmoji("\uD83D\uDD25")
-                                                ),
-                                                RoleMessageSelectMenuChoice(
-                                                    roleName = "V2",
-                                                    label = "V2",
-                                                    emoji = UnicodeEmoji("\uD83D\uDE15")
-                                                ),
-                                            )
+                                )
+                            ),
+                            RoleMessageRow(
+                                components = arrayListOf(
+                                    RoleMessageSelectMenu(
+                                        placeholder = null,
+                                        choices = arrayListOf(
+                                            RoleMessageSelectMenuChoice(
+                                                roleName = "V3",
+                                                label = "V3",
+                                                emoji = UnicodeEmoji("\uD83D\uDD25")
+                                            ),
+                                            RoleMessageSelectMenuChoice(
+                                                roleName = "V2",
+                                                label = "V2",
+                                                emoji = UnicodeEmoji("\uD83D\uDE15")
+                                            ),
                                         )
                                     )
                                 )
@@ -62,7 +60,7 @@ class RolesConfigTests(
                     )
                 )
             )
-        }
+        )
 
         assertDoesNotThrow { rolesConfigRepository.findByGuildId(GUILD_ID) }
     }
