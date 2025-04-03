805865f7093e4da6f51b1ee0dc3a8199560b28a5
freya02 <41875020+freya022@users.noreply.github.com>
2025-04-03 17:36:14 +0200

    backend: Downgrade to Kotlin 2.1.0
    Got some weird random dependency resolution issues with 2.1.20

--- backend/buildSrc/build.gradle.kts
+++ backend/buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 
 dependencies {
     // Change in version catalog too
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     implementation("com.google.cloud.tools:jib-gradle-plugin:3.4.4")
 }
