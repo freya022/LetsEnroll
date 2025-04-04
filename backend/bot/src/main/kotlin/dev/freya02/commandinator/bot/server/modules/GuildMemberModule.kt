2d19b365f5bc1e88d5770981e728ff5dd90e4a5d
freya02 <41875020+freya022@users.noreply.github.com>
2025-04-04 17:21:57 +0200

    bot: Use Ktor "modules"

---
+++ backend/bot/src/main/kotlin/dev/freya02/commandinator/bot/server/modules/GuildMemberModule.kt
@@ -0,0 +1,43 @@
+package dev.freya02.commandinator.bot.server.modules
+
+import dev.freya02.commandinator.api.dto.MemberDTO
+import dev.freya02.commandinator.bot.server.resources.Guilds
+import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.utils.retrieveMemberByIdOrNull
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.resources.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import net.dv8tion.jda.api.JDA
+
+@BService
+object GuildMemberModule : KtorModule {
+
+    private val logger = KotlinLogging.logger { }
+
+    override fun Application.defineModule(jda: JDA) {
+        routing {
+            getGuildMember(jda)
+        }
+    }
+
+    private fun Route.getGuildMember(jda: JDA) = get<Guilds.Id.Members.Id> { memberResource ->
+        val guildId = memberResource.members.guild.guildId
+        val userId = memberResource.userId
+        val guild = jda.getGuildById(guildId)
+        if (guild == null) {
+            logger.debug { "No guild found for $guildId" }
+            return@get call.respond(HttpStatusCode.NotFound)
+        }
+
+        val member = guild.retrieveMemberByIdOrNull(userId)
+        if (member == null) {
+            logger.debug { "No member found for $userId in guild $guildId" }
+            return@get call.respond(HttpStatusCode.NotFound)
+        }
+
+        call.respond(MemberDTO(member.permissions.mapTo(hashSetOf()) { it.name }))
+    }
+}
