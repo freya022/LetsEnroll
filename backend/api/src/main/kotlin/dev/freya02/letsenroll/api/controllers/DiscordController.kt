f3d574602bef5e5d4eb9df66bdf7bf8932c88f38
freya02 <41875020+freya022@users.noreply.github.com>
2025-04-14 19:25:58 +0200

    api: Forbid fetching channels from guilds the user is not in

--- backend/api/src/main/kotlin/dev/freya02/letsenroll/api/controllers/DiscordController.kt
+++ backend/api/src/main/kotlin/dev/freya02/letsenroll/api/controllers/DiscordController.kt
@@ -2,12 +2,18 @@ package dev.freya02.letsenroll.api.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import dev.freya02.letsenroll.api.bot.BotClient
+import dev.freya02.letsenroll.api.bot.isInGuild
 import dev.freya02.letsenroll.api.discord.DiscordClient
+import dev.freya02.letsenroll.api.utils.DashboardUser
+import dev.freya02.letsenroll.api.utils.discordId
 import dev.freya02.letsenroll.api.utils.getDiscordAuthorizationHeader
+import org.springframework.http.HttpStatus
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 class DiscordController(
@@ -30,7 +36,10 @@ class DiscordController(
     }
 
     @GetMapping("/api/guilds/{guildId}/channels")
-    fun getGuildChannels(@PathVariable guildId: Long): String {
+    fun getGuildChannels(@PathVariable guildId: Long, @DashboardUser user: OAuth2User): String {
+        if (!botClient.isInGuild(guildId, user.discordId))
+            throw ResponseStatusException(HttpStatus.FORBIDDEN)
+
         return botClient.getGuildChannels(guildId)
     }
 }
---
+++ backend/api/src/test/kotlin/dev/freya02/letsenroll/api/DiscordControllerTests.kt
@@ -0,0 +1,43 @@
+package dev.freya02.letsenroll.api
+
+import com.ninjasquad.springmockk.MockkBean
+import dev.freya02.letsenroll.api.bot.BotClient
+import dev.freya02.letsenroll.api.bot.isInGuild
+import dev.freya02.letsenroll.api.controllers.DiscordController
+import dev.freya02.letsenroll.api.discord.DiscordClient
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@WebMvcTest(controllers = [DiscordController::class])
+class DiscordControllerTests @Autowired constructor(
+    private val mockMvc: MockMvc,
+) {
+
+    @MockkBean
+    lateinit var botClient: BotClient
+
+    @MockkBean
+    lateinit var discordClient: DiscordClient
+
+    @Test
+    fun `Must be in guild to get channels`() {
+        mockkStatic(BotClient::isInGuild) // Top-level extensions are static
+        every { botClient.isInGuild(any(), any()) } returns false
+        every { botClient.getGuildChannels(any()) } returns ""
+
+        mockMvc.get("/api/guilds/$EXAMPLE_GUILD_ID/channels") {
+            withLoggedInInvalidUser()
+        }.andExpect {
+            status { isForbidden() }
+        }
+
+        verify(exactly = 1) { botClient.isInGuild(any(), any()) }
+        verify(exactly = 0) { botClient.getGuildChannels(any()) }
+    }
+}
