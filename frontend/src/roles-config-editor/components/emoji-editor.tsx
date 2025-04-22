c5c649daba6de20b859bf290d578fa6e80580bed
freya02 <41875020+freya022@users.noreply.github.com>
2025-04-22 17:13:12 +0200

    front: Refetch custom emojis on window focus

--- frontend/src/roles-config-editor/components/emoji-editor.tsx
+++ frontend/src/roles-config-editor/components/emoji-editor.tsx
@@ -53,7 +53,8 @@ export function EmojiEditor<T extends EmojiContainer>({
       const response = await axios.get(`/api/guilds/${guildId}/emojis`);
       return response.data as CustomEmojiCandidate[];
     },
-    refetchOnMount: "always",
+    // The user could add an emoji and then come back here
+    refetchOnWindowFocus: "always",
   });
 
   const [open, setOpen] = useState(false);
