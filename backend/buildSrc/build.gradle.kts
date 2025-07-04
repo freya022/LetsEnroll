d4dccbee751127bb0c0e364a56b195a8ce3bd444
freya02 <41875020+freya022@users.noreply.github.com>
2025-07-04 18:28:17 +0200

    back: Set JVM compat to 21 for precompiled scripts

--- backend/buildSrc/build.gradle.kts
+++ backend/buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `kotlin-dsl`
 }
@@ -12,4 +14,20 @@ dependencies {
     // Change in version catalog too
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     implementation("com.google.cloud.tools:jib-gradle-plugin:3.4.4")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        // Do with what Gradle supports
+        jvmTarget = JvmTarget.JVM_21
+
+        freeCompilerArgs.addAll(
+            "-Xjsr305=strict",
+        )
+    }
 }
