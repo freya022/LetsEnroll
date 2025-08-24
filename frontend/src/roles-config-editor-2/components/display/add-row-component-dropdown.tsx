ef900e8a3e8518141860f68c9f014c4456de9b08
freya02 <41875020+freya022@users.noreply.github.com>
2025-08-24 16:26:25 +0200

    front: Support adding buttons in rows

--- frontend/src/roles-config-editor-2/components/display/add-row-component-dropdown.tsx
+++ frontend/src/roles-config-editor-2/components/display/add-row-component-dropdown.tsx
@@ -1,14 +1,24 @@
 import AddComponentDropdown from "@/roles-config-editor-2/components/display/add-component-dropdown.tsx";
+import { useRolesConfigDispatch } from "@/roles-config-editor-2/hooks/roles-config-context.ts";
+import { RowData } from "@/roles-config-editor-2/types/component-data.ts";
 
-export default function AddRowComponentDropdown() {
+export default function AddRowComponentDropdown({ row }: { row: RowData }) {
+  const dispatch = useRolesConfigDispatch();
+
   return (
     <AddComponentDropdown
       label="Add component..."
       items={[
         {
           label: "Toggle button",
-          action: () => {
-            console.log("button");
+          action: (event) => {
+            event.stopPropagation();
+
+            dispatch!({
+              type: "edit",
+              fn: (draft) =>
+                draft.find(row)!.components.push(draft.newButton()),
+            });
           },
         },
       ]}
