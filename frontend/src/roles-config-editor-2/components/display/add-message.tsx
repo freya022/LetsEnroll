7b1265a4ce71db1585af7bccc11d03afcfcaf722
freya02 <41875020+freya022@users.noreply.github.com>
2025-08-24 16:32:20 +0200

    front: Supporting adding messages

--- frontend/src/roles-config-editor-2/components/display/add-message.tsx
+++ frontend/src/roles-config-editor-2/components/display/add-message.tsx
@@ -1,6 +1,19 @@
+import { useRolesConfigDispatch } from "@/roles-config-editor-2/hooks/roles-config-context.ts";
+
 export default function AddMessage() {
+  const dispatch = useRolesConfigDispatch();
+
+  function handleAddMessage() {
+    dispatch!({
+      type: "add_message",
+    });
+  }
+
   return (
-    <button className="flex h-16 w-md cursor-pointer items-center justify-center rounded-[8px] border-[2px] border-dashed border-gray-600 px-[11px] py-[3px]">
+    <button
+      onClick={handleAddMessage}
+      className="flex h-16 w-md cursor-pointer items-center justify-center rounded-[8px] border-[2px] border-dashed border-gray-600 px-[11px] py-[3px]"
+    >
       <span className="text-sm">Add message</span>
     </button>
   );
