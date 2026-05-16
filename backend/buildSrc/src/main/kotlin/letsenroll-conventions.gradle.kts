0765e0b04b73ec595909c20518c409f0aa7dd9e7
freya02 <41875020+freya022@users.noreply.github.com>
2026-05-16 13:02:04 +0200

    back. build: Set toolchain to Java 25

--- backend/buildSrc/src/main/kotlin/letsenroll-conventions.gradle.kts
+++ backend/buildSrc/src/main/kotlin/letsenroll-conventions.gradle.kts
@@ -9,17 +9,12 @@ version = "1.0"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(24)
+        languageVersion = JavaLanguageVersion.of(25)
     }
-
-    sourceCompatibility = JavaVersion.VERSION_24
-    targetCompatibility = JavaVersion.VERSION_24
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_24
-
         freeCompilerArgs.addAll(
             "-Xjsr305=strict",
         )
